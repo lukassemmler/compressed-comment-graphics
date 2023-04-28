53ef9937a7bed569ce6e895c14a0c11f71ecfae3
lukassemmler <kontakt@lukassemmler.de>
2023-04-29 01:08:07 +0200

    Add brightness change detection

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var PNG = require('png-js');
+
+const Brightness = Object.freeze({
+  Light: 1,
+  Dark: 0,
+});
+
 PNG.decode('media/nintendo-land.png', function(pixels) {
   const pixelsArray = new Uint8Array(pixels);
+  let brightnessChanges = 0;
+  let currentBrightness = -1;
+  let currentLength = -1;
   for (let i = 0; i < pixelsArray.length; i +=4 ) {
     const pixel = pixelsArray.slice(i, i + 4);
     const [ r, g, b, a ] = pixel;
     const brightness = Math.round((r + g + b) / 3);
-    const type = brightness > 127 ? "light" : "dark";
-    console.log(type);
-    break;
+    const type = brightness > 127 ? Brightness.Light : Brightness.Dark;
+    if (type === currentBrightness) {
+      currentLength++;
+    } else {
+      currentBrightness = type;
+      currentLength = 1;
+      brightnessChanges++;
+    }
   }
+  console.log(brightnessChanges);
   //const brightness = 
   // pixels is a 1d array (in rgba order) of decoded pixel data
 });
