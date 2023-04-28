0338ec52dbd009a16a41ad74fe847c76e8f609ba
lukassemmler <kontakt@lukassemmler.de>
2023-04-29 01:28:19 +0200

    Add bit handling (wip)

--- index.js
+++ index.js
@@ -5,23 +5,44 @@ const Brightness = Object.freeze({
   Dark: 0,
 });
 
+function getRepeaterChunkByte(stack) {
+  
+}
+
+function getRawChunkByte(stack) {
+
+}
+
 PNG.decode('media/nintendo-land.png', function(pixels) {
   const pixelsArray = new Uint8Array(pixels);
-  let brightnessChanges = 0;
+  //let brightnessChanges = 0;
+  let lastFlushIndex = -1;
   let currentBrightness = -1;
-  let currentLength = -1;
+  let currentStreak = -1;
   for (let i = 0; i < pixelsArray.length; i +=4 ) {
     const pixel = pixelsArray.slice(i, i + 4);
     const [ r, g, b, a ] = pixel;
     const brightness = Math.round((r + g + b) / 3);
     const type = brightness > 127 ? Brightness.Light : Brightness.Dark;
     if (type === currentBrightness) {
-      currentLength++;
+      currentStreak++;
+      // Case 3: Streak with more than 64 bits
+      //   gets broken up and written as a repeater chunk
+      getRepeaterChunkByte(stack);
     } else {
       currentBrightness = type;
-      currentLength = 1;
-      brightnessChanges++;
+      currentStreak = 1;
+      //brightnessChanges++;
+      // Case 2: Streak with at least 8 bits
+      //   gets written as a repeater chunk
+      getRepeaterChunkByte(stack);
     }
+    if (stack.length < 7) continue;
+    if (stack.length === currentStreak) continue;
+    // Case 1: Random sequence of 7 bits 
+    //   get written as a raw chunk
+    //const rawBits = 
+    getRawChunkByte(stack);
   }
   console.log(brightnessChanges);
   //const brightness = 
