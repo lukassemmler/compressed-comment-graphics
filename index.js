1530a4988906297476b026d2f0cdee2e25542576
lukassemmler <kontakt@lukassemmler.de>
2023-04-29 16:02:27 +0200

    Add functions for chunk bytes

--- index.js
+++ index.js
@@ -5,8 +5,32 @@ const Brightness = Object.freeze({
   Dark: 0,
 });
 
-function getRepeaterChunkByte(stack) {
-  
+function getRepeaterChunkByte(bit, length) {
+  // Returns 1 + bit + uint6 (8 bits or 1 byte)
+  //   ATTENTION! We don't care about repeaters with a length of 0, so we always add 1.
+  //   So a repeater has a length between 1 - 64 (2^6) -- and NOT 0 - 63 (2^6 - 1).
+  if (length < 1 || length > 64)
+    throw new Error(`Illegal repeater chunk byte length of '${length}'. Must be between 1 and 64 (including 1 and 64).`);
+  let byte = 1;
+  byte <<= 1;
+  byte |= bit;
+  byte <<= 6
+  byte |= length - 1;
+  return byte;
+}
+
+function getRawChunkByte(bits) {
+  // Returns 0 + sequence of 7 bits (8 bits or 1 byte)
+  // TODO: Change bits array to a 7-bit number to be more memory efficient?
+  const bitLength = bits.length;
+  if (bitLength < 1 || bitLength > 7)
+    throw new Error(`Illegal raw chunk byte length of '${bitLength}' bits. Must be between 1 and 7 bits (including 1 and 7).`);
+  let byte = 0;
+  for (let i = 0; i < 7; i++) {
+    byte <<= 1
+    byte |= bits[i];
+  }
+  return byte;
 }
 
 function getRawChunkByte(stack) {
